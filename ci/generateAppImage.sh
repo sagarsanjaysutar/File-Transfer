0fce8b566ece82182ccc82368d3ba0f759367ca0
Sagar S Sutar <sutarsagarsanjay@gmail.com>
2024-08-24 11:29:17 +0530

    Misc cleanup

--- ci/generateAppImage.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export QML_SOURCES_PATHS=/File-Transfer/res/qml
-export QMAKE=/opt/Qt/6.5.0/gcc_64/bin/qmake
-export LD_LIBRARY_PATH=/opt/Qt/6.5.0/gcc_64/lib
-/home/user/linux-deploy.AppImage --appdir /File-Transfer/build/AppDir/ --output appimage --plugin qt
